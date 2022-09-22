1457961d4c4adc8b8424d42a428735dd6a6a7c64
jnyjhow <jhonatan_yamane@hotmail.com>
2022-09-22 15:30:44 -0300

    pagination

--- frontend/src/components/CreateLink.js
+++ frontend/src/components/CreateLink.js
@@ -3,6 +3,8 @@ import { useMutation, gql } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 import { FEED_QUERY } from "./LinkList";
 
+import { AUTH_TOKEN, LINKS_PER_PAGE } from "../constants";
+
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
     post(description: $description, url: $url) {
@@ -28,8 +30,17 @@ const CreateLink = () => {
       url: formState.url,
     },
     update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createdAt: "desc" };
+
       const data = cache.readQuery({
         query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
       });
 
       cache.writeQuery({
@@ -39,9 +50,16 @@ const CreateLink = () => {
             links: [post, ...data.feed.links],
           },
         },
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
       });
     },
-    onCompleted: () => navigate("/"),
+    onCompleted: () => {
+      navigate("/");
+    },
   });
 
   return (
